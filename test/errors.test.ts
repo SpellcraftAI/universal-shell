e94bad6db3fc61213fb550490c7eebff0c896ea4
C. Lewis <ctj.lewis@icloud.com>
2022-02-03 21:52:49 -0600

    chore: update timeout

--- test/errors.test.ts
+++ test/errors.test.ts
@@ -2,7 +2,7 @@ import { shell, killShell } from "../src";
 import test from "ava";
 
 
-const SLEEP = process.platform === "win32" ? "timeout /t 30 /nobreak > NUL" : "sleep 30";
+const SLEEP = "node -e 'const start = Date.now(); while(Date.now() - start < 30000) {};'";
 
 test.serial("should not throw for exit code 0", async (t) => {
   if (process.platform !== "win32") {
@@ -54,22 +54,19 @@ test.serial("should throw for nonzero exit code", async (t) => {
 test.serial("killShell() should kill subprocesses", async (t) => {
   t.timeout(10_000);
 
-  const { exec } = await import("child_process");
-  exec("echo test");
-
-  // await Promise.allSettled([
-  //   shell(`${SLEEP} & ${SLEEP} & echo Slept 30s.`),
-  //   new Promise(
-  //     (resolve) => {
-  //       setTimeout(
-  //         () => {
-  //           const killed = killShell();
-  //           resolve(killed);
-  //         },
-  //         5000
-  //       );
-  //     }),
-  // ]);
+  await Promise.allSettled([
+    shell(`${SLEEP} & ${SLEEP} & echo Slept 30s.`),
+    new Promise(
+      (resolve) => {
+        setTimeout(
+          () => {
+            const killed = killShell();
+            resolve(killed);
+          },
+          5000
+        );
+      }),
+  ]);
 
   t.pass();
 });
